dddc4448b66641eadf8fdcd0131fe591728bd6df
xuwei-k <6b656e6a69@gmail.com>
2017-10-18 10:16:01 +0900

    Scala 2.12.4

--- project/Build.scala
+++ project/Build.scala
@@ -38,7 +38,7 @@ object build {
 
   lazy val commonSettings = ReleasePlugin.extraReleaseCommands ++ Seq(
     name := msgpack4zNativeName,
-    crossScalaVersions := Scala211 :: "2.12.3" :: "2.13.0-M2" :: Nil,
+    crossScalaVersions := Scala211 :: "2.12.4" :: "2.13.0-M2" :: Nil,
     commands += Command.command("updateReadme")(UpdateReadme.updateReadmeTask),
     resolvers += Opts.resolver.sonatypeReleases,
     fullResolvers ~= {_.filterNot(_.name == "jcenter")},
