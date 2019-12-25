571ebd2d2986f67163b85d1768117682eb730e85
xuwei-k <6b656e6a69@gmail.com>
2019-12-25 11:37:59 +0900

    remove unused val in build file

--- project/Build.scala
+++ project/Build.scala
@@ -3,9 +3,4 @@ object build {
   val msgpack4zNativeName = "msgpack4z-native"
   val modules = msgpack4zNativeName :: Nil
 
-  private[this] val unusedWarnings = (
-    "-Ywarn-unused" ::
-    Nil
-  )
-
 }
