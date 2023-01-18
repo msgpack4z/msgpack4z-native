364ddb05f31918bddd7de72e5b780ef6f6fcfe4a
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2023-01-18 23:00:44 +0000

    Reformat with scalafmt 3.7.0
    Executed command: scalafmt --non-interactive

--- project/UpdateReadme.scala
+++ project/UpdateReadme.scala
@@ -7,37 +7,38 @@ object UpdateReadme {
 
   private val sonatypeURL = "https://oss.sonatype.org/service/local/repositories/"
 
-  val updateReadmeTask = { state: State =>
-    val extracted = Project.extract(state)
-    val scalaV = extracted get scalaBinaryVersion
-    val v = extracted get version
-    val org = extracted get organization
-    val modules = build.modules
-    val snapshotOrRelease = if (extracted get isSnapshot) "snapshots" else "releases"
-    val readme = "README.md"
-    val readmeFile = file(readme)
-    val newReadme = Predef
-      .augmentString(IO.read(readmeFile))
-      .lines
-      .map { line =>
-        val matchReleaseOrSnapshot = line.contains("SNAPSHOT") == v.contains("SNAPSHOT")
-        def n = modules(modules.indexWhere(line.contains))
-        if (line.startsWith("libraryDependencies") && matchReleaseOrSnapshot) {
-          s"""libraryDependencies += "${org}" %% "$n" % "$v""""
-        } else line
+  val updateReadmeTask = {
+    state: State =>
+      val extracted = Project.extract(state)
+      val scalaV = extracted get scalaBinaryVersion
+      val v = extracted get version
+      val org = extracted get organization
+      val modules = build.modules
+      val snapshotOrRelease = if (extracted get isSnapshot) "snapshots" else "releases"
+      val readme = "README.md"
+      val readmeFile = file(readme)
+      val newReadme = Predef
+        .augmentString(IO.read(readmeFile))
+        .lines
+        .map { line =>
+          val matchReleaseOrSnapshot = line.contains("SNAPSHOT") == v.contains("SNAPSHOT")
+          def n = modules(modules.indexWhere(line.contains))
+          if (line.startsWith("libraryDependencies") && matchReleaseOrSnapshot) {
+            s"""libraryDependencies += "${org}" %% "$n" % "$v""""
+          } else line
+        }
+        .mkString("", "\n", "\n")
+      IO.write(readmeFile, newReadme)
+      val git = new Git(extracted get baseDirectory)
+      val logger = new scala.sys.process.ProcessLogger {
+        def buffer[T](f: => T): T = f
+        def err(s: => String): Unit = state.log.error(s)
+        def out(s: => String): Unit = state.log.info(s)
       }
-      .mkString("", "\n", "\n")
-    IO.write(readmeFile, newReadme)
-    val git = new Git(extracted get baseDirectory)
-    val logger = new scala.sys.process.ProcessLogger {
-      def buffer[T](f: => T): T = f
-      def err(s: => String): Unit = state.log.error(s)
-      def out(s: => String): Unit = state.log.info(s)
-    }
-    git.add(readme) ! logger
-    git.commit(message = "update " + readme, sign = false, signOff = false) ! logger
-    Process("git diff HEAD^") ! state.log
-    state
+      git.add(readme) ! logger
+      git.commit(message = "update " + readme, sign = false, signOff = false) ! logger
+      Process("git diff HEAD^") ! state.log
+      state
   }
 
   val updateReadmeProcess: ReleaseStep = updateReadmeTask
